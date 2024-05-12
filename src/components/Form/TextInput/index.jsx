d24d52250094ce88f12cff7c4a3854a585718717
jorik <spiridonovjar@gmail.com>
2024-05-12 16:13:45 +0400

    fixes

--- src/components/Form/TextInput/index.jsx
+++ src/components/Form/TextInput/index.jsx
@@ -5,7 +5,8 @@ import frame from "@assets/input-field.png";
 
 const TextInput = forwardRef(({ placeholder, error, ...props }, ref) => {
    const bright = {
-      filter: "drop-shadow(rgb(255, 0, 0) 0px 0px) saturate(1.5)",
+      filter: "drop-shadow(rgb(255, 0, 0) 0px 0px) saturate(1.7)",
+      WebkitFilter: "drop-shadow(rgb(255, 0, 0) 0px 0px) saturate(1.7)",
    };
 
    return (
